13374bf05d9da471c8cd3d50679174dd43865cef
Jules Lai <juleslai1234@gmail.com>
2023-03-20 05:08:47 +0000

    Update  transcoder and queue code

--- transcode_server/src/server.rs
+++ transcode_server/src/server.rs
@@ -1,13 +1,27 @@
+/*
+ * server.rs
+ *
+ * This file contains code for transcoding a video using ffmpeg.
+ * Upload a video in h264 format and it will be transcoded to 2 h264 mp4 files;
+ * one in 1080p format and another in 720p.
+ * This is then uploaded to decentralised SIA Storage via S5.
+ *
+ * Author: Jules Lai
+ * Date: 1 March 2023
+ */
+
 mod s5;
 use s5::{download_file, upload_video};
 
 use tonic::{transport::Server, Request, Response, Status};
 
+use async_trait::async_trait;
 use once_cell::sync::Lazy;
 use sanitize_filename::sanitize;
 use std::process::Command;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use tokio::sync::mpsc;
+use tokio::sync::Mutex;
 use transcode::{
     transcode_service_server::{TranscodeService, TranscodeServiceServer},
     GetCidRequest, GetCidResponse, TranscodeRequest, TranscodeResponse,
@@ -22,32 +36,19 @@ static PATH_TO_FILE: &str = "path/to/file/";
 
 // The transcoding task receiver, which receives transcoding tasks from the gRPC server
 async fn transcode_task_receiver(receiver: Arc<Mutex<mpsc::Receiver<String>>>) {
-    let mut receiver = receiver.lock().unwrap();
-    loop {
-        match receiver.try_recv() {
-            Ok(file_path) => {
-                println!("Transcoding video: {}", &file_path);
-                if let Err(e) = transcode_video(&file_path) {
-                    eprintln!("Failed to transcode {}: {}", &file_path, e);
-                }
-            }
-            Err(e) => {
-                if e == mpsc::error::TryRecvError::Empty {
-                    continue;
-                } else {
-                    eprintln!("Failed to receive transcoding task: {}", e);
-                    break;
-                }
-            }
+    while let Some(file_path) = receiver.lock().await.recv().await {
+        println!("Transcoding video: {}", &file_path);
+        if let Err(e) = transcode_video(&file_path).await {
+            eprintln!("Failed to transcode {}: {}", &file_path, e);
         }
     }
 }
 
 // Transcodes a video file to 1080p and 720p h264 mp4 formats using ffmpeg
-fn transcode_video(url: &str) -> Result<Response<TranscodeResponse>, Status> {
+async fn transcode_video(url: &str) -> Result<Response<TranscodeResponse>, Status> {
     println!("Downloading video from: {}", url);
 
-    let mut video_cid = VIDEO_CID.lock().unwrap();
+    let mut video_cid = VIDEO_CID.lock().await;
     *video_cid = url.to_string();
 
     let file_name = sanitize(url);
@@ -120,7 +121,7 @@ fn transcode_video(url: &str) -> Result<Response<TranscodeResponse>, Status> {
                 &cid
             );
 
-            let mut video_cid1 = VIDEO_CID1.lock().unwrap();
+            let mut video_cid1 = VIDEO_CID1.lock().await;
             *video_cid1 = cid;
 
             response = TranscodeResponse {
@@ -150,7 +151,7 @@ fn transcode_video(url: &str) -> Result<Response<TranscodeResponse>, Status> {
                 &cid
             );
 
-            let mut video_cid2 = VIDEO_CID2.lock().unwrap();
+            let mut video_cid2 = VIDEO_CID2.lock().await;
             *video_cid2 = cid;
         }
         Err(e) => {
@@ -169,10 +170,10 @@ fn transcode_video(url: &str) -> Result<Response<TranscodeResponse>, Status> {
 // The gRPC service implementation
 #[derive(Debug, Clone)]
 struct TranscodeServiceHandler {
-    transcode_task_sender: Option<mpsc::Sender<String>>,
+    transcode_task_sender: Option<Arc<Mutex<mpsc::Sender<String>>>>,
 }
 
-#[tonic::async_trait]
+#[async_trait]
 impl TranscodeService for TranscodeServiceHandler {
     async fn transcode(
         &self,
@@ -189,6 +190,7 @@ impl TranscodeService for TranscodeServiceHandler {
         if let Some(ref sender) = self.transcode_task_sender {
             println!("Before: if let Err(e) = sender.send(file_path).await");
 
+            let sender = sender.lock().await;
             if let Err(e) = sender.send(url).await {
                 return Err(Status::internal(format!(
                     "Failed to send transcoding task: {}",
@@ -213,8 +215,8 @@ impl TranscodeService for TranscodeServiceHandler {
 
         // Assuming `resolution` is already defined and contains the resolution value
         let cidOption = match resolution {
-            "1080p" => Some(VIDEO_CID1.lock().unwrap().to_string()),
-            "720p" => Some(VIDEO_CID2.lock().unwrap().to_string()),
+            "1080p" => Some(VIDEO_CID1.lock().await.to_string()),
+            "720p" => Some(VIDEO_CID2.lock().await.to_string()),
             _ => None,
         };
 
@@ -259,6 +261,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Create a gRPC server
     let addr = "0.0.0.0:50051".parse()?;
+
+    // Wrap task_sender in an Arc<Mutex<>> before passing it to TranscodeServiceHandler
+    let task_sender = Arc::new(Mutex::new(task_sender));
+
     let transcode_service_handler = TranscodeServiceHandler {
         transcode_task_sender: Some(task_sender),
     };
